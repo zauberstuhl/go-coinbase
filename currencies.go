359428073bf7e881be00e27b050b47dcc515bbab
Lukas Matt <lukas@zauberstuhl.de>
2017-01-23 23:44:43 +0100

    Add currencies/exchange_rates api implementation

---
+++ currencies.go
@@ -0,0 +1,64 @@
+/*
+ * Coinbase Golang API Library
+ *
+ * Copyright (C) 2017 Lukas Matt <lukas@zauberstuhl.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package coinbase
+
+/*
+ * {
+ *   "data": [
+ *     {
+ *       "id": "AED",
+ *       "name": "United Arab Emirates Dirham",
+ *       "min_size": "0.01000000"
+ *     },
+ *     {
+ *       "id": "AFN",
+ *       "name": "Afghan Afghani",
+ *       "min_size": "0.01000000"
+ *     },
+ *     {
+ *       "id": "ALL",
+ *       "name": "Albanian Lek",
+ *       "min_size": "0.01000000"
+ *     },
+ *     {
+ *       "id": "AMD",
+ *       "name": "Armenian Dram",
+ *       "min_size": "0.01000000"
+ *     },
+ *     ...
+ *   }
+ * }
+ */
+type APICurrenciesData struct {
+  Id string
+  Name string
+  Min_size float64
+}
+type APICurrencies struct {
+  Data []APICurrenciesData
+}
+// Get currencies
+func (a *APIClient) GetCurrencies() (curr APICurrencies, err error) {
+  err = a.Fetch("GET", "/v2/currencies", nil, &curr)
+  if err != nil {
+    return
+  }
+  return
+}
---
+++ exchange_rates.go
@@ -0,0 +1,57 @@
+/*
+ * Coinbase Golang API Library
+ *
+ * Copyright (C) 2017 Lukas Matt <lukas@zauberstuhl.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package coinbase
+
+/*
+ * {
+ *   "data": {
+ *     "currency": "BTC",
+ *     "rates": {
+ *       "AED": "36.73",
+ *       "AFN": "589.50",
+ *       "ALL": "1258.82",
+ *       "AMD": "4769.49",
+ *       "ANG": "17.88",
+ *       "AOA": "1102.76",
+ *       "ARS": "90.37",
+ *       "AUD": "12.93",
+ *       "AWG": "17.93",
+ *       "AZN": "10.48",
+ *       "BAM": "17.38",
+ *       ...
+ *     }
+ *   }
+ * }
+ */
+type APIExchangeRatesData struct {
+  Currency string
+  Rates interface{}
+}
+type APIExchangeRates struct {
+  Data []APIExchangeRatesData
+}
+// Get exchange rates
+func (a *APIClient) GetExchangeRates(currency string) (rates APIExchangeRates, err error) {
+  err = a.Fetch("GET", "/v2/exchange-rates?currency=" + currency, nil, &rates)
+  if err != nil {
+    return
+  }
+  return
+}
