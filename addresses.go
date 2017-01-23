b56d6f4353f49515ea93fdaa219ed9418966d824
Lukas Matt <lukas@zauberstuhl.de>
2017-01-23 23:27:46 +0100

    Add address/transaction api implementation

---
+++ addresses.go
@@ -0,0 +1,90 @@
+/*
+ * Coinbase Golang API Library
+ *
+ * Copyright (C) 2017 Lukas Matt <lukas@zauberstuhl.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package coinbase
+
+import "bytes"
+
+/*
+ * {
+ *   "id": "dd3183eb-af1d-5f5d-a90d-cbff946435ff",
+ *   "address": "mswUGcPHp1YnkLCgF1TtoryqSc5E9Q8xFa",
+ *   "name": "One off payment",
+ *   "created_at": "2015-01-31T20:49:02Z",
+ *   "updated_at": "2015-03-31T17:25:29-07:00",
+ *   "network": "bitcoin",
+ *   "resource": "address",
+ *   "resource_path": "/v2/accounts/2bbf394c-193b-5b2a-9155-3b4732659ede/addresses/dd3183eb-af1d-5f5d-a90d-cbff946435ff"
+ * }
+ */
+type APIAddressData struct {
+  Id string
+  Address string
+  Name string
+  Created_at string
+  Updated_at string
+  Network string
+  Resource string
+  Resource_path string
+}
+type APIAddresses struct {
+  Pagination APIPagination
+  Data []APIAddressData
+}
+type APIAddress struct {
+  Data APIAddressData
+}
+// List addresses
+func (a *APIClient) GetAddresses(id string) (addresses APIAddresses, err error) {
+  err = a.Fetch("GET", "/v2/accounts/" + id + "/addresses", nil, &addresses)
+  if err != nil {
+    return
+  }
+  return
+}
+
+// Show address
+func (a *APIClient) GetAddress(id, addressId string) (address APIAddress, err error) {
+  err = a.Fetch("GET", "/v2/accounts/" + id + "/addresses/" + addressId, nil, &address)
+  if err != nil {
+    return
+  }
+  return
+}
+
+// List address’s transactions
+func (a *APIClient) ListAddressTransactions(id, address string) (trans APITransactions, err error) {
+  path := "/v2/accounts/" + id + "/addresses/" + address + "/transactions"
+  err = a.Fetch("GET", path, nil, &trans)
+  if err != nil {
+    return
+  }
+  return
+}
+
+// Create address
+func (a *APIClient) CreateAddress(id, name string) (address APIAddress, err error) {
+  var body = []byte("{\"name\": \"" + name + "\"}")
+  err = a.Fetch("POST", "/v2/accounts/" + id + "/addresses",
+    bytes.NewBuffer(body), &address)
+  if err != nil {
+    return
+  }
+  return
+}
---
+++ transactions.go
@@ -0,0 +1,166 @@
+/*
+ * Coinbase Golang API Library
+ *
+ * Copyright (C) 2017 Lukas Matt <lukas@zauberstuhl.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package coinbase
+
+import (
+  "bytes"
+  "encoding/json"
+)
+
+/*
+ * {
+ *   "id": "57ffb4ae-0c59-5430-bcd3-3f98f797a66c",
+ *   "type": "send",
+ *   "status": "completed",
+ *   "amount": {
+ *     "amount": "-0.00100000",
+ *     "currency": "BTC"
+ *   },
+ *   "native_amount": {
+ *     "amount": "-0.01",
+ *     "currency": "USD"
+ *   },
+ *   "description": null,
+ *   "created_at": "2015-03-11T13:13:35-07:00",
+ *   "updated_at": "2015-03-26T15:55:43-07:00",
+ *   "resource": "transaction",
+ *   "resource_path": "/v2/accounts/2bbf394c-193b-5b2a-9155-3b4732659ede/transactions/57ffb4ae-0c59-5430-bcd3-3f98f797a66c",
+ *   "network": {
+ *     "status": "off_blockchain",
+ *     "name": "bitcoin"
+ *   },
+ *   "to": {
+ *     "id": "a6b4c2df-a62c-5d68-822a-dd4e2102e703",
+ *     "resource": "user",
+ *     "resource_path": "/v2/users/a6b4c2df-a62c-5d68-822a-dd4e2102e703"
+ *   },
+ *   "instant_exchange": false,
+ *   "details": {
+ *     "title": "Sent bitcoin",
+ *     "subtitle": "to User 2"
+ *   }
+ * }
+ */
+type APITransactionsDataDetails struct {
+  Title string
+  Subtitle string
+}
+type APITransactionsDataToBuy struct {
+  Id string
+  Resource string
+  Resource_path string
+}
+type APITransactionsDataNetwork struct {
+  Status string
+  Name string
+}
+type APITransactionsData  struct {
+  Id string
+  Type string
+  Status string
+  Amount APIBalance
+  Native_amount APIBalance
+  Description string
+  Created_at string
+  Updated_at string
+  Resource string
+  Resource_path string
+  Network APITransactionsDataNetwork
+  To APITransactionsDataToBuy
+  Buy APITransactionsDataToBuy
+  Instant_exchange bool
+  Details APITransactionsDataDetails
+}
+type APITransactions struct {
+  Pagination APIPagination
+  Data []APITransactionsData
+}
+type APITransaction struct {
+  Data APITransactionsData
+}
+// List transactions
+func (a *APIClient) GetTransactions(id string) (trans APITransactions, err error) {
+  err = a.Fetch("GET", "/v2/accounts/" + id + "/transactions", nil, &trans)
+  if err != nil {
+    return
+  }
+  return
+}
+
+// Show transaction
+func (a *APIClient) GetTransaction(id, transId string) (trans APITransaction, err error) {
+  path := "/v2/accounts/" + id + "/transactions/" + transId
+  err = a.Fetch("GET", path, nil, &trans)
+  if err != nil {
+    return
+  }
+  return
+}
+
+type APITransactionsSend struct {
+  Type string
+  To string
+  Amount float64
+  Currency string
+}
+// Send/Transfer/Request money
+func (a *APIClient) SendTransferRequestMoney(id string, send APITransactionsSend) (trans APITransaction, err error) {
+  data, err := json.Marshal(send)
+  if err != nil {
+    return
+  }
+  // TODO implement idem
+  err = a.Fetch("POST", "/v2/accounts/" + id + "/transactions",
+    bytes.NewBuffer([]byte(data)), &trans)
+  if err != nil {
+    return
+  }
+  return
+}
+
+// Complete request money
+func (a *APIClient) CompleteRequestMoney(id, transId string) (err error) {
+  path := "/v2/accounts/" + id + "/transactions/" + transId + "/complete"
+  err = a.Fetch("GET", path, nil, nil)
+  if err != nil {
+    return
+  }
+  return
+}
+
+// Re-send request money
+func (a *APIClient) ResendRequestMoney(id, transId string) (err error) {
+  path := "/v2/accounts/" + id + "/transactions/" + transId + "/resend"
+  err = a.Fetch("GET", path, nil, nil)
+  if err != nil {
+    return
+  }
+  return
+}
+
+// Cancel request money
+func (a *APIClient) CancelRequestMoney(id, transId string) (err error) {
+  path := "/v2/accounts/" + id + "/transactions/" + transId
+  err = a.Fetch("DELETE", path, nil, nil)
+  if err != nil {
+    return
+  }
+  return
+}
