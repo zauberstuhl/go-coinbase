910693a80f06a622df02dc636cfdf958f8ccb098
Lukas Matt <lukas@zauberstuhl.de>
2017-01-26 13:42:53 +0100

    Add prices unit test

---
+++ test/prices_test.go
@@ -0,0 +1,68 @@
+package coinbase_test
+
+import (
+  "testing"
+  "zauberstuhl/coinbase"
+  "time"
+)
+
+func TestGetBuyPrice(t *testing.T) {
+  a := coinbase.APIClient{}
+  balance, err := a.GetBuyPrice(
+    coinbase.ConfigPrice{
+      From: "BTC",
+      To: "EUR",
+  })
+  if err != nil {
+    t.Error("Expected nil, got ", err.Error())
+  }
+  if balance.Data.Currency != "EUR" {
+    t.Error("Expected EUR, got ", balance.Data.Currency)
+  }
+  if balance.Data.Amount <= 0.0 {
+    t.Error("Expected <= 0.0, got ", balance.Data.Amount)
+  }
+}
+
+func TestGetSellPrice(t *testing.T) {
+  a := coinbase.APIClient{}
+  balance, err := a.GetSellPrice(
+    coinbase.ConfigPrice{
+      From: "BTC",
+      To: "EUR",
+  })
+  if err != nil {
+    t.Error("Expected nil, got ", err.Error())
+  }
+  if balance.Data.Currency != "EUR" {
+    t.Error("Expected EUR, got ", balance.Data.Currency)
+  }
+  if balance.Data.Amount <= 0.0 {
+    t.Error("Expected <= 0.0, got ", balance.Data.Amount)
+  }
+}
+
+func TestGetSpotPrice(t *testing.T) {
+  a := coinbase.APIClient{}
+  date, err := time.Parse("2006-01-02", "2017-01-01")
+  if err != nil {
+    t.Error("Expected nil, got ", err.Error())
+  }
+
+  balance, err := a.GetSpotPrice(
+    coinbase.ConfigPrice{
+      From: "BTC",
+      To: "EUR",
+      Date: date,
+  })
+  if err != nil {
+    t.Error("Expected nil, got ", err.Error())
+  }
+  if balance.Data.Currency != "EUR" {
+    t.Error("Expected EUR, got ", balance.Data.Currency)
+  }
+  if balance.Data.Amount != 912.81 {
+    t.Error("Expected <= 912.81, got ", balance.Data.Amount)
+  }
+}
+
