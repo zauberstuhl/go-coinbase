13a8dda685b6b7b7ea54502b88256919d69f08e6
Lukas Matt <lukas@zauberstuhl.de>
2017-01-26 03:06:25 +0100

    Add exchange_rates unit test

---
+++ test/exchange_rates_test.go
@@ -0,0 +1,32 @@
+package coinbase_test
+
+import (
+  "testing"
+  "zauberstuhl/coinbase"
+)
+
+func TestGetExchangeRates(t *testing.T) {
+  a := coinbase.APIClient{
+    Key: "123",
+    Secret: "123456",
+  }
+  exch, err := a.GetExchangeRates("BTC")
+  if err != nil {
+    t.Error("Expected nil, got ", err.Error())
+  }
+  if len(exch.Data.Currency) != 3 {
+    t.Error("Expected currency length equal three")
+  }
+  for key, value := range exch.Data.Rates {
+    if len(key) != 3 {
+      t.Error("Expected ", key, " length equal three")
+    }
+    rate, err := value.Float64()
+    if err != nil {
+      t.Error("Expected nil, got ", err.Error())
+    }
+    if rate <= 0.0 {
+      t.Error("Expected value to be greater then zero")
+    }
+  }
+}
