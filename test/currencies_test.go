dcd05c529719ea5f5550866c3a0e04ef70fd22f8
Lukas Matt <lukas@zauberstuhl.de>
2017-01-26 02:31:36 +0100

    Add currencies unit test

---
+++ test/currencies_test.go
@@ -0,0 +1,31 @@
+package coinbase_test
+
+import (
+  "testing"
+  "zauberstuhl/coinbase"
+)
+
+func TestGetCurrencies(t *testing.T) {
+  a := coinbase.APIClient{
+    Key: "123",
+    Secret: "123456",
+  }
+  list, err := a.GetCurrencies()
+  if err != nil {
+    t.Error("Expected nil, got ", err.Error())
+  }
+  if len(list.Data) <= 0 {
+    t.Error("Expected list to be greater then zero")
+  }
+  for i, curr := range list.Data {
+    if len(curr.Id) != 3 {
+      t.Error(i, ". Expected id length equal three")
+    }
+    if len(curr.Name) <= 0 {
+      t.Error(i, ". Expected name length to be greater then zero")
+    }
+    if curr.Min_size <= 0.0 {
+      t.Error(i, ". Expected min_size to be greater then zero")
+    }
+  }
+}
