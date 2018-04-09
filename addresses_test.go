4de925a479a57c2201f798d055c33f051d311da7
Lukas Matt <lukas@zauberstuhl.de>
2018-04-09 14:16:07 +0200

    Add more unit tests
    * Accounts
    * Addresses
    * Buys

--- addresses_test.go
+++ addresses_test.go
@@ -4,21 +4,22 @@ import (
   "testing"
 )
 
-func TestAddresses(t *testing.T) {
-  s := serverHelper(`
- {
-   "data": {
-       "id": "dd3183eb-af1d-5f5d-a90d-cbff946435ff",
-       "address": "mswUGcPHp1YnkLCgF1TtoryqSc5E9Q8xFa",
-       "name": "One off payment",
-       "created_at": "2015-01-31T20:49:02Z",
-       "updated_at": "2015-03-31T17:25:29-07:00",
-       "network": "bitcoin",
-       "resource": "address",
-       "resource_path": "/v2/accounts/2bbf394c-193b-5b2a-9155-3b4732659ede/addresses/dd3183eb-af1d-5f5d-a90d-cbff946435ff"
-   }
- }
-  `)
+func TestAddress(t *testing.T) {
+  method := "GET"
+  uri := "/v2/accounts/dd3183eb-af1d-5f5d-a90d-cbff946435ff/addresses/mswUGcPHp1YnkLCgF1TtoryqSc5E9Q8xFa"
+  body := `{
+    "data": {
+        "id": "dd3183eb-af1d-5f5d-a90d-cbff946435ff",
+        "address": "mswUGcPHp1YnkLCgF1TtoryqSc5E9Q8xFa",
+        "name": "One off payment",
+        "created_at": "2015-01-31T20:49:02Z",
+        "updated_at": "2015-03-31T17:25:29-07:00",
+        "network": "bitcoin",
+        "resource": "address",
+        "resource_path": "/v2/accounts/2bbf394c-193b-5b2a-9155-3b4732659ede/addresses/dd3183eb-af1d-5f5d-a90d-cbff946435ff"
+    }
+  }`
+  s := serverHelper(t, &method, &uri, &body)
   defer s.Close()
 
   a := APIClient{
@@ -27,25 +28,36 @@ func TestAddresses(t *testing.T) {
     Endpoint: s.URL,
   }
 
-  address, err := a.GetAddress(
+  add, err := a.GetAddress(
     "dd3183eb-af1d-5f5d-a90d-cbff946435ff",
-    "mswUGcPHp1YnkLCgF1TtoryqSc5E9Q8xFa",
-  )
+    "mswUGcPHp1YnkLCgF1TtoryqSc5E9Q8xFa")
   if err != nil {
     t.Error("Expected nil, got ", err.Error())
   }
-  if address.Data.Id != "dd3183eb-af1d-5f5d-a90d-cbff946435ff" {
-    t.Error("Expected dd3183eb-af1d-5f5d-a90d-cbff946435ff, got ",
-      address.Data.Id)
+  address(t, add.Data)
+
+  method = "POST"
+  uri = "/v2/accounts/mswUGcPHp1YnkLCgF1TtoryqSc5E9Q8xFa/addresses"
+  add, err = a.CreateAddress(
+    "mswUGcPHp1YnkLCgF1TtoryqSc5E9Q8xFa",
+    "One off payment")
+  if err != nil {
+    t.Error("Expected nil, got ", err.Error())
   }
-  if address.Data.Address != "mswUGcPHp1YnkLCgF1TtoryqSc5E9Q8xFa" {
-    t.Error("Expected mswUGcPHp1YnkLCgF1TtoryqSc5E9Q8xFa, got ",
-      address.Data.Address)
+  address(t, add.Data)
+}
+
+func address(t *testing.T, address APIAddressData) {
+  if address.Id != "dd3183eb-af1d-5f5d-a90d-cbff946435ff" {
+    t.Error("Expected dd3183eb-af1d-5f5d-a90d-cbff946435ff, got ", address.Id)
   }
-  if address.Data.Created_at.Year() != 2015 {
-    t.Error("Expected 2015, got ", address.Data.Created_at.Year())
+  if address.Address != "mswUGcPHp1YnkLCgF1TtoryqSc5E9Q8xFa" {
+    t.Error("Expected mswUGcPHp1YnkLCgF1TtoryqSc5E9Q8xFa, got ", address.Address)
   }
-  if address.Data.Updated_at.Year() != 2015 {
-    t.Error("Expected 2015, got ", address.Data.Created_at.Year())
+  if address.Created_at.Year() != 2015 {
+    t.Error("Expected 2015, got ", address.Created_at.Year())
+  }
+  if address.Updated_at.Year() != 2015 {
+    t.Error("Expected 2015, got ", address.Created_at.Year())
   }
 }
--- buys_test.go
+++ buys_test.go
@@ -5,47 +5,48 @@ import (
 )
 
 func TestBuys(t *testing.T) {
-  s := serverHelper(`
-{
-  "data": {
-    "id": "9e14d574-30fa-5d85-b02c-6be0d851d61d",
-    "status": "created",
-    "payment_method": {
-      "id": "83562370-3e5c-51db-87da-752af5ab9559",
-      "resource": "payment_method",
-      "resource_path": "/v2/payment-methods/83562370-3e5c-51db-87da-752af5ab9559"
-    },
-    "transaction": {
-      "id": "4117f7d6-5694-5b36-bc8f-847509850ea4",
-      "resource": "transaction",
-      "resource_path": "/v2/accounts/2bbf394c-193b-5b2a-9155-3b4732659ede/transactions/441b9494-b3f0-5b98-b9b0-4d82c21c252a"
-    },
-    "amount": {
-      "amount": "10.00000000",
-      "currency": "BTC"
-    },
-    "total": {
-      "amount": "102.01",
-      "currency": "USD"
-    },
-    "subtotal": {
-      "amount": "101.00",
-      "currency": "USD"
-    },
-    "created_at": "2015-03-26T23:43:59-07:00",
-    "updated_at": "2015-03-26T23:44:09-07:00",
-    "resource": "buy",
-    "resource_path": "/v2/accounts/2bbf394c-193b-5b2a-9155-3b4732659ede/buys/9e14d574-30fa-5d85-b02c-6be0d851d61d",
-    "committed": true,
-    "instant": false,
-    "fee": {
-      "amount": "1.01",
-      "currency": "USD"
-    },
-    "payout_at": "2015-04-01T23:43:59-07:00"
-  }
-}
-  `)
+  method := "GET"
+  uri := "/v2/accounts/123/buys/456"
+  body := `{
+    "data": {
+      "id": "9e14d574-30fa-5d85-b02c-6be0d851d61d",
+      "status": "created",
+      "payment_method": {
+        "id": "83562370-3e5c-51db-87da-752af5ab9559",
+        "resource": "payment_method",
+        "resource_path": "/v2/payment-methods/83562370-3e5c-51db-87da-752af5ab9559"
+      },
+      "transaction": {
+        "id": "4117f7d6-5694-5b36-bc8f-847509850ea4",
+        "resource": "transaction",
+        "resource_path": "/v2/accounts/2bbf394c-193b-5b2a-9155-3b4732659ede/transactions/441b9494-b3f0-5b98-b9b0-4d82c21c252a"
+      },
+      "amount": {
+        "amount": "10.00000000",
+        "currency": "BTC"
+      },
+      "total": {
+        "amount": "102.01",
+        "currency": "USD"
+      },
+      "subtotal": {
+        "amount": "101.00",
+        "currency": "USD"
+      },
+      "created_at": "2015-03-26T23:43:59-07:00",
+      "updated_at": "2015-03-26T23:44:09-07:00",
+      "resource": "buy",
+      "resource_path": "/v2/accounts/2bbf394c-193b-5b2a-9155-3b4732659ede/buys/9e14d574-30fa-5d85-b02c-6be0d851d61d",
+      "committed": true,
+      "instant": false,
+      "fee": {
+        "amount": "1.01",
+        "currency": "USD"
+      },
+      "payout_at": "2015-04-01T23:43:59-07:00"
+    }
+  }`
+  s := serverHelper(t, &method, &uri, &body)
   defer s.Close()
 
   a := APIClient{
